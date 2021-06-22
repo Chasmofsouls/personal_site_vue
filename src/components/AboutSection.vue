8e5ad66a6aa00b9bf82115e8e52e982891bf82b0
Chasmofsouls <ayomidealonge15@gmail.com>
2021-06-22 13:15:03 +0100

    personalsite

---
+++ src/components/AboutSection.vue
@@ -0,0 +1,69 @@
+<template>
+  <section id="about">
+    <v-container fluid>
+      <v-row align="center" justify="center">
+        <v-col cols="10">
+          <v-row align="center" justify="center">
+            <v-col cols="12" md="7">
+              <h1 class="font-weight-light display-2">About</h1>
+              <h1 class="font-weight-light display-1 mb-3">
+                Below is a little of what i can say about myself!
+              </h1>
+              <v-row>
+                <v-col cols="12" class="d-flex align-center">
+                  <v-img
+                    src="@/assets/img/icon1.svg"
+                    max-width="60px"
+                    class="mr-4"
+                  />
+                  <p class="text-justify">
+                    As a web developer, i enjoy using my obsessive attention to detail.
+                    I ensure to give a good value of services for a very considerable price.
+                    I am mission-driven and deadline adherent.
+                  </p>
+                </v-col>
+                <v-col cols="12" class="d-flex align-center">
+                  <v-img
+                    src="@/assets/img/icon2.svg"
+                    max-width="60px"
+                    class="mr-4"
+                  />
+                  <p class="text-justify">
+                    My mission-driven work ethic is to design, develop and deploy
+                    user desirable, technically viable and economically feasible services
+                    in a bid to make this planet a smarter and better environment.
+                  </p>
+                </v-col>
+                <v-col cols="12" class="d-flex align-center">
+                  <v-img
+                    src="@/assets/img/icon3.svg"
+                    max-width="60px"
+                    class="mr-4"
+                  />
+                  <p class="text-justify">
+                    I am proficient in the use of HTML, CSS, Javascript, Vuejs, Jquery
+                    and a little bit of Nodejs for my client-side development. I use
+                    Figma, AdobeXD and my creative abilities for User Interfaces and Experience.
+                  </p>
+                </v-col>
+              </v-row>
+            </v-col>
+            <v-col cols="12" md="5" class="d-none d-md-flex">
+              <v-img
+                src="@/assets/img/ill1.svg"
+                class="d-block ml-auto mr-auto"
+                max-width="400px"
+              />
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-container>
+  </section>
+</template>
+
+<style scoped>
+#about {
+  background-color: #f4f7f5;
+}
+</style>
